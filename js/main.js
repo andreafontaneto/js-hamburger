bd37862a9dbc605271228b2a903cfaa3b3b71b1c
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-22 15:18:03 +0200

    definizione variabili

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.getElementsByClassName('fa-bars');
+const hamburgerMenuList = document.getElementsByClassName('hamburger-menu');
+const close = document.getElementsByClassName('close');
+
+
+
+
